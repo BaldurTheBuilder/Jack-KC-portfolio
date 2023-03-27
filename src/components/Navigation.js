5b5cdf7db121707aabc8f8ff7b20ef77cb3ab7ff
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-03-26 18:10:51 -0700

    header css being constructed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@
 // WHEN I view the navigation titles I see the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
 // WHEN I click on a navigation title I see the corresponding section below the navigation without the page reloading and that title is highlighted
 import React from 'react';
+import '../styles/Header.css';
 
 const Navigation = ({currentPage, handlePageChange}) => {
     return (
