118d502051494f0415b580f2c4ed3dc293d07f03
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-03-26 21:05:55 -0700

    profile pic added

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import '../../styles/Page.css';
 
+// WHEN I am presented with the ______About Me_____ section I see a recent photo or avatar of the developer and a short bio about them
 export default function AboutMe() {
     return (
-        <p>About Me page</p>
+        <div>
+            <h4>About Me </h4>
+            <img className="profile" src="profile.jpg" alt="text"/>
+        </div>
+
     );
 }
